27e8585519353cb92813e906ceffa2bf46f03f76
michsethowusu <michsethowusu@gmail.com>
2026-04-05 09:21:25 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 set -e
-flask init-db 2>/dev/null || python -c "
+
+python -c "
 from app import app, db
 with app.app_context():
     db.create_all()
-    print('DB tables created.')
+    # Add new columns to existing tables if they don't exist yet
+    with db.engine.connect() as conn:
+        conn.execute(db.text('''
+            ALTER TABLE \"user\"
+            ADD COLUMN IF NOT EXISTS password_hash VARCHAR(256)
+        '''))
+        conn.commit()
+    print('DB ready.')
 "
+
 exec gunicorn -w 4 -b 0.0.0.0:3000 app:app

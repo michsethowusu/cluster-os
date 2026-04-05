99d279b496406d02427f96f55a0d6cacf20c6bf2
michsethowusu <michsethowusu@gmail.com>
2026-04-05 22:17:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ with app.app_context():
     db.create_all()
     # Add new columns to existing tables if they don't exist yet
     with db.engine.connect() as conn:
+        # Update User table
         conn.execute(db.text('''
             ALTER TABLE \"user\"
             ADD COLUMN IF NOT EXISTS password_hash VARCHAR(256)
@@ -15,6 +16,21 @@ with app.app_context():
             ALTER TABLE \"user\"
             ADD COLUMN IF NOT EXISTS points INTEGER DEFAULT 0
         '''))
+        
+        # Update Project table (Fixes your 500 error)
+        conn.execute(db.text('''
+            ALTER TABLE project
+            ADD COLUMN IF NOT EXISTS is_published BOOLEAN DEFAULT FALSE
+        '''))
+        conn.execute(db.text('''
+            ALTER TABLE project
+            ADD COLUMN IF NOT EXISTS start_date TIMESTAMP
+        '''))
+        conn.execute(db.text('''
+            ALTER TABLE project
+            ADD COLUMN IF NOT EXISTS submitted_by INTEGER
+        '''))
+
         conn.commit()
     print('DB ready.')
 "
